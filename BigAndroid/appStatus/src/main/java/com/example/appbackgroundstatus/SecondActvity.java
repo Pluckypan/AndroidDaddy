ab0c4c985c4edbbb271f989fc2b13373ad75695d
plucky <plucky.pan@ubnt.com>
2017-05-23 23:59:30 +0800

    background listener

---
+++ BigAndroid/appStatus/src/main/java/com/example/appbackgroundstatus/SecondActvity.java
@@ -0,0 +1,13 @@
+package com.example.appbackgroundstatus;
+
+import android.os.Bundle;
+
+public class SecondActvity extends BaseActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+	}
+
+}
