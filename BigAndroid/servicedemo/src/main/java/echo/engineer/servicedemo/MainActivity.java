9008c7052017237689fbc218de3048e03430403d
Plucky <plucky.pan@ubnt.com>
2017-06-22 14:23:59 +0800

    upgrade ide version

---
+++ BigAndroid/servicedemo/src/main/java/echo/engineer/servicedemo/MainActivity.java
@@ -0,0 +1,73 @@
+package echo.engineer.servicedemo;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btnStartService).setOnClickListener(this);
+        findViewById(R.id.btnStopService).setOnClickListener(this);
+        findViewById(R.id.btnBindService).setOnClickListener(this);
+        findViewById(R.id.btnUnBindService).setOnClickListener(this);
+    }
+
+
+    public void onStartService(View view) {
+        Intent intentDownload = new Intent(this, DownloadService.class);
+        startService(intentDownload);
+    }
+
+    public void onStopService(View view) {
+        stopService(new Intent(this, DownloadService.class));
+    }
+
+    public void onBindService(View view) {
+        Intent intent = new Intent(this, DownloadService.class);
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+    }
+
+    public void onUnBindService(View view) {
+        if (serviceConnection != null) {
+            unbindService(serviceConnection);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStartService:
+                onStartService(v);
+                break;
+            case R.id.btnStopService:
+                onStopService(v);
+                break;
+            case R.id.btnBindService:
+                onBindService(v);
+                break;
+            case R.id.btnUnBindService:
+                onUnBindService(v);
+                break;
+        }
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+}
