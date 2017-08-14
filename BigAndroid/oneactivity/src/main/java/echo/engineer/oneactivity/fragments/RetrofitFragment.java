c53029d8eee08b5e2511e1e35f5b60db0b6d835f
plucky <py@meitu.com>
2017-08-14 13:49:27 +0800

    add retrofit2

---
+++ BigAndroid/oneactivity/src/main/java/echo/engineer/oneactivity/fragments/RetrofitFragment.java
@@ -0,0 +1,12 @@
+package echo.engineer.oneactivity.fragments;
+
+import com.fragmentmaster.app.MasterFragment;
+
+/**
+ * RetrofitFragment.java
+ * Useage: RetrofitFragment
+ * Created by Plucky<py@meitu.com> on 2017/8/14 - 13:45
+ */
+
+public class RetrofitFragment extends MasterFragment {
+}
