d556f5b0f6c3cfdeb67c88e9e9a15eaae437dfd0
plucky <plucky.pan@ubnt.com>
2017-11-14 00:13:07 +0800

    研究RecyclerView

---
+++ BigAndroid/recyclerviewstudy/src/main/java/engineer/echo/recyclerviewstudy/MainActivity.java
@@ -0,0 +1,15 @@
+package engineer.echo.recyclerviewstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //http://mp.weixin.qq.com/s/VQGx1_Q-7No0gXficmjujw
+        //https://github.com/leochuan/ViewPagerLayoutManager
+    }
+}
