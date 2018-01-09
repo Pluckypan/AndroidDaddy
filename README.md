9e17bf2129b17053ffecad093b6dae5c6db0fdd5
plucky <plucky.pan@ubnt.com>
2018-01-09 23:31:27 +0800

    整理项目结构

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 > 你爸爸还是你爸爸  解读Android前沿技术代码RxJava,Dagger,OkHttp,Annotation,IPC,智能硬件
 
 ### 子项目
-- Dagger 研究Google推荐项目 Dagger
-- FloatingWindow 无权限实现悬浮窗,实现日志打印功能
-- Keeper 智能手表项目
-- RxJava 响应式编程、自由切换线程、观察者模式
-- ServiceDemo 简单的进程间通信
+- **android.apis** `Google Android API` 示例研究
+- **dagger** 研究`Google`推荐项目 `Dagger`
+- **floatingWindow** 无权限实现悬浮窗,实现日志打印功能
+- **keeper** 智能手表项目
+- **rxjava** 响应式编程、自由切换线程、观察者模式
+- **service** `service` 基础应用及扩展研究
+- **app**  杂项
+- **oneActivity** 基于[OneActivity](https://github.com/Pluckypan/OneActivity)
---
+++ service/src/main/java/echo/engineer/service/MessageHandler.java
@@ -0,0 +1,34 @@
+package echo.engineer.service;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * MessageHandler
+ * Created by Plucky<plucky.pan@ubnt.com> on 7/5/17 2017 14:02.
+ */
+
+public class MessageHandler extends Handler {
+    private static final String TAG = "MessageHandler";
+
+    @Override
+    public void handleMessage(Message msg) {
+        String receiveMsg = msg.getData().getString("msg");
+        Log.i(TAG, "Server handleMessage msg=" + receiveMsg);
+        AppStateManager.publishMsgState(receiveMsg);
+        Messenger messenger = msg.replyTo;
+        Message message = Message.obtain(null, 0);
+        Bundle bundle = new Bundle();
+        bundle.putString("reply", "receive");
+        message.setData(bundle);
+        try {
+            messenger.send(message);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
