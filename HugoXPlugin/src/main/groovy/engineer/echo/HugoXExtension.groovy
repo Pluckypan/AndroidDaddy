c52d689d1e262d1447b13a8477a2ac6690f0c39f
plucky <py@meitu.com>
2018-12-20 21:44:56 +0800

    [feature] hugox plugin 1.1.0

--- HugoXPlugin/src/main/groovy/engineer/echo/HugoXExtension.groovy
+++ HugoXPlugin/src/main/groovy/engineer/echo/HugoXExtension.groovy
@@ -1,5 +1,7 @@
 package engineer.echo
 
 class HugoXExtension {
+    //开关
     boolean enable = true
+    String version = "1.0.1"
 }
--- HugoXPlugin/src/main/groovy/engineer/echo/HugoXPlugin.groovy
+++ HugoXPlugin/src/main/groovy/engineer/echo/HugoXPlugin.groovy
@@ -6,11 +6,19 @@ import org.gradle.api.Project
 class HugoXPlugin implements Plugin<Project> {
 
     void apply(Project project) {
+        //创建HugoX扩展,可以配置开关
         project.extensions.create("HugoX", HugoXExtension)
-        def enable = project.HugoX.enable
-        println("HugoXPlugin enable=$enable")
+        def enable = false, ver = "xxx"
+        project.afterEvaluate {
+            //获取HugoX配置的参数
+            def extension = project.HugoX
+            enable = extension.enable
+            ver = extension.version
+            println("HugoXPlugin enable=$enable ver=$ver")
+        }
+        //创建任务
         project.task('showHugoXInfo') << {
-            println "HugoX switch on= $enable"
+            println "HugoX switch on= $enable ver=$ver"
         }
     }
 }
